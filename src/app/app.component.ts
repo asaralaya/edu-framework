eba0e258f30cf12694b1586018012db5b0daee1f
Abhishek P N <abhishekpn98@gmail.com>
2023-06-23 00:28:34 +0530

    analytic enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { inject } from "@vercel/analytics";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'edu-framework';
+  ngOnInit(){
+    inject()
+  }
 }
